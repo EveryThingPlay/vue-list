400694cfe1287024c986a828d837b78b90a77957
EveryThingPlay <kokochiha30@gmail.com>
2024-05-02 11:38:30 +0300

    Небольшой фикс дергающихся блоков при ховере

--- src/components/CListBlock.vue
+++ src/components/CListBlock.vue
@@ -19,16 +19,22 @@ watch(() => props.val, () => {
 </script>
 
 <template>
-  <div ref="block" class="element">
-    {{ val }}
+  <div class="wrap">
+    <div ref="block" class="element">
+      {{ val }}
+    </div>
   </div>
 </template>
 
 <style lang="pcss" scoped>
 .element {
-  @apply flex justify-center items-center text-black font-semibold hover:scale-80 duration-150 hover:duration-75 h-12 w-12 bg-blue-300 cursor-default rounded-md;
+  @apply flex justify-center items-center text-black font-semibold duration-150 hover:duration-75 h-12 w-12 bg-blue-300 cursor-default rounded-md;
 }
-
+.wrap:hover {
+  .element {
+    @apply scale-80;
+  }
+}
 .animate {
   @apply bg-red-500 duration-100 scale-110;
 }
